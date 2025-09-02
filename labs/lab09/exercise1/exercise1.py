ac3304c93511e68135284dc5f1c4f1ead4ab49c5
christiangeronimo243@gmail.com <christiangeronimo243gmail.com>
2025-09-02 11:57:26 +0800

    lab 09

--- labs/lab09/exercise1/exercise1.py
+++ labs/lab09/exercise1/exercise1.py
@@ -5,9 +5,16 @@ gpa = float(input("Enter GPA (0.0-4.0): "))
 credit_hours = int(input("Enter credit hours: "))
 
 # TODO your code here
+# if statement - always at the beginning
+student_grade = 85
 
+if student_grade >= 90:
+    print("Excellent performance")
+    
+percentage = (marks / total_marks) * 100
+print(f"Student scored: {percentage}%")
 # Display results
 print(f"\nStudent: {student_name}")
 print(f"GPA: {gpa}")
 print(f"Credit Hours: {credit_hours}")
-print(f"Classification: {classification}")
+#print(f"Classification: {classification}")
