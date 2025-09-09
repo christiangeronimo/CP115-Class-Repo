1e6b11b908670bd35f32a108a9c18640dbfbd226
christiangeronimo243@gmail.com <christiangeronimo243gmail.com>
2025-09-09 11:30:32 +0800

    lab 09

--- labs/lab09/exercise1/exercise1.py
+++ labs/lab09/exercise1/exercise1.py
@@ -5,10 +5,22 @@ gpa = float(input("Enter GPA (0.0-4.0): "))
 credit_hours = int(input("Enter credit hours: "))
 
 # TODO your code here
+# multiple conditions with elif
 
+if gpa >= 3.8 and credit_hours >= 12 :
+    classification = "Dean's List"
+elif gpa >= 3.5 and credit_hours >= 12: 
+    classification = "Honor Roll"
+elif gpa >= 2.0: 
+    classification = "Good Standing"
+elif gpa < 2.0:
+    classification = "Academic Probation"
+elif credit_hours <12 :
+    classification = "Part-time students"
+else:
+    classification = "Fail"
     
-percentage = (marks / total_marks) * 100
-print(f"Student scored: {percentage}%")
+
 # Display results
 print(f"\nStudent: {student_name}")
 print(f"GPA: {gpa}")
--- labs/lab09/exercise2/exercise2.py
+++ labs/lab09/exercise2/exercise2.py
@@ -4,8 +4,25 @@ overtime_hours = int(input())
 tax_status = input()
 
 # TODO your code here
+if tax_status == "Single" and base_salary >= 5000:
+    tax_rate= 22
+else: 
+    tax_rate = 18
 
+if tax_status == "Married" and base_salary >= 6000:
+    tax_rate = 0.20/100
+else: 
+    tax_rate = 0.15/100
 
+if tax_status == "Head" and base_salary >= 5500:
+    tax_rate = 0.25/100
+else: 
+    tax_rate = 0.19/100
+
+EPF = 0.11/100
+SOCSO = 0.005/100
+net_salary = ((tax_rate + EPF + SOCSO) * base_salary)+ (35 * overtime_hours) 
+ 
 print(employee_name)
 print(tax_rate)
 print(f"{net_salary:.2f}")
