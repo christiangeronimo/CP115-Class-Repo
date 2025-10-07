3116e55bf3249bf4388d4554cb737c79b707d927
christiangeronimo243@gmail.com <christiangeronimo243gmail.com>
2025-10-07 12:35:51 +0800

    Complete Lab 11: Counter-Controlled Loops with Exercises

--- labs/lab11/exercise3/exercise3.py
+++ labs/lab11/exercise3/exercise3.py
@@ -1,7 +1,12 @@
 target_points = int(input())
+total_points = 0
+rounds_played = 0
 
-# TODO: Your code here
-# Use input() inside the while loop to get points each round
+# Keep playing rounds until total_points reaches or exceeds target_points
+while total_points < target_points:
+    points = int(input())
+    total_points += points
+    rounds_played += 1
 
 print(total_points)
 print(rounds_played)
